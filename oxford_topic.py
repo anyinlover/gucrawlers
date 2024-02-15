8de32e62cb70b832771cbb22b8ee9521727a8a43
guhangsong <guhangsong@outlook.com>
2024-02-15 21:35:40 +0800

    modify oxford_topic

--- oxford_topic.py
+++ oxford_topic.py
@@ -1,15 +1,35 @@
 import re
-from scrapy.spiders import SitemapSpider
+import scrapy
 
-
-class TopicSpider(SitemapSpider):
+class TopicSpider(scrapy.Spider):
     name = "topic_spider"
-    sitemap_urls = ["https://www.oxfordlearnersdictionaries.com/sitemap/sitemap_topic.xml"]
 
-    def sitemap_filter(self, entries):
-        for entry in entries:
-            url = entry["loc"]
-            if re.fullmatch(r'https://www.oxfordlearnersdictionaries.com/topic/\w+', url):
-                yield entry 
-
+    @classmethod
+    def update_settings(cls, settings):
+        super().update_settings(settings)
+        settings.set("DOWNLOAD_HANDLERS", {"http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+                    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler"}, priority="spider")
+        settings.set("TWISTED_REACTOR", "twisted.internet.asyncioreactor.AsyncioSelectorReactor", priority="spider")
+        settings.set("USER_AGENT", None, priority="spider")
+        settings.set("PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT", 1000*1000, priority="spider")
     
+    def start_requests(self):
+        url = "https://www.oxfordlearnersdictionaries.com/sitemap/sitemap_topic.xml"
+        yield scrapy.Request(url, self.parse_sitemap)
+
+    def parse_sitemap(self, response):
+        for url in response.css("loc::text"):
+            if re.fullmatch(r'https://www.oxfordlearnersdictionaries.com/topic/\w+', url):
+                yield scrapy.Request(url, meta={"playwright": True})
+
+    def parse(self, response):
+        topic = response.css("h1::text").get()
+        lists = response.css("ul.top-g")
+        for item in lists.css("li"):
+            yield {
+                "topic": topic,
+                "word": item.css("a::text").get(),
+                "url": item.css("a::attr(href)").get(),
+                "pos": item.css("span.pos::text").get(),
+                "belong": item.css("span.belong-to::text").get(),
+            }
